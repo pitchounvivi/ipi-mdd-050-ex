bb4d71d2957bb731699ed7d5a2643ebaa32add6e
pitchounvivi <pitchounvivi@gmail.com>
2020-11-17 12:31:59 +0100

    ajout commentaire utilisation méthode countEmploye

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -15,6 +15,10 @@ public class EmployeController {
     @Autowired
     private EmployeRepository employeRepository;
 
+    // Cette méthode est utilisée pour être appliquée à l'adresse :
+    // file:///D:/ipi/java/ipi-web-050-web/index.html
+    // et
+    // http://localhost:5367/employes/count
     @RequestMapping(
             value = "/count",
             method = RequestMethod.GET,
