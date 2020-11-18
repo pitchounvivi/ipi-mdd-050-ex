696f327ab6884c76f2d543e64339d641e3ffa5f9
pitchounvivi <pitchounvivi@gmail.com>
2020-11-18 14:15:51 +0100

    création de l'url

--- src/main/java/com/ipiecoles/java/mdd050/controller/ManagerController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/ManagerController.java
@@ -1,10 +1,22 @@
 package com.ipiecoles.java.mdd050.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/managers")
 public class ManagerController {
-    
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{idManager}/equipe/{idTechnicien}/remove"
+    )
+    public void removeTechnicienFromEquipe(
+            @PathVariable Long idManager,
+            @PathVariable Long idTechnicien
+    ){
+
+    }
 }
